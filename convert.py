ccdd9c3fddbe87289292e91d89998fdecc21f601
chaush-server <79080580+chaush-server@users.noreply.github.com>
2026-07-11 23:27:48 +0300

    Enhance generate_srs_domains to accept regexes

--- convert.py
+++ convert.py
@@ -2,8 +2,17 @@ import json
 import subprocess
 
 
-def generate_srs_domains(domains: list[str]):
-    data = {"version": 3, "rules": [{"domain_suffix": domains}]}
+def generate_srs_domains(domains: list[str], domain_regexes: list[str]):
+    rules = []
+    if domains:
+        rules.append({"domain_suffix": domains})
+    if domain_regexes:
+        rules.append({"domain_regex": domain_regexes})
+
+    data = {
+        "version": 3,
+        "rules": rules,
+    }
     srs_file_path = "domains.srs"
     json_file_path = "domains.json"
     try:
@@ -14,21 +23,25 @@ def generate_srs_domains(domains: list[str]):
             ["sing-box", "rule-set", "compile", json_file_path, "-o", srs_file_path],
             check=True,
         )
-    except subprocess.CalledProcessError as e:
-        print(f"Compile error {json_file_path}: {e}")
-    except Exception as e:
-        print(f"Error while processing {domains}: {e}")
+    except subprocess.CalledProcessError as error:
+        print(f"Compile error {json_file_path}: {error}")
+    except Exception as error:
+        print(f"Error while processing {domains}: {error}")
 
 
 def main():
     domains = []
-    with open("domains.lst") as f:
-        for line in f.readlines():
+    domain_regexes = []
+    with open("domains.lst") as domain_list:
+        for line in domain_list:
             domain = line.strip()
             if domain and domain[0] not in ["/", "#"]:
-                domains.append(domain)
-    
-    generate_srs_domains(domains)
+                if domain.startswith("^") and domain.endswith("$"):
+                    domain_regexes.append(domain)
+                else:
+                    domains.append(domain)
+
+    generate_srs_domains(domains, domain_regexes)
 
 
 if __name__ == "__main__":
